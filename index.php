9457e17720c63cac7e543d17828474e71caee56d
ayuukg83-ship-it <ayuukg83@gmail.com>
2026-07-21 20:01:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Ajax ShopEase</title>
+  <title>Teams</title>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js"></script>
   <style>
     * { box-sizing: border-box; }
